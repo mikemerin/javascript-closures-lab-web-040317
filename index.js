94fe641a70062b27f29761d5b61811f2baed5b9b
Mike Merin <mikemerinweather@gmail.com>
2017-05-17 17:03:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+function bumpCounter() {
+
+  var counter = 0
+
+  function addBump() { ++counter }
+  function getBumps() { return counter }
+
+  return {addBump, getBumps}
+
+}
+
+
+function createAnimal(animalType) {
+  return (deadlyDevice) => { return { animalType, deadlyDevice } }
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+// sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
+// sharkWithFrickinCannon = createAnimal("Shark")("Cannon")
